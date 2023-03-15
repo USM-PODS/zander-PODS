b51e5a21dafb5f4abd126c0d7f51bc9ebbd53dca
Zander Solak <zander.solak@gmail.com>
2023-03-15 12:25:28 -0400

    moved data module

--- congealFunction.jl
+++ congealFunction.jl
@@ -3,62 +3,49 @@ using DataFrames
 
 include("DataModulePODS.jl")
 
-buoyID = "44007" #the ID number for the buoy
+#!buoyID = "44007" #the ID number for the buoy
 txt = ".txt" #matches the file type of the text files from NDBC
 h = "h" #This is an h, its just a part of the names from the NDBC maybe it means hourly?
 undscore ="_" #For new naming convention
 commasep = ".csv" #used to write out files as comma seperated
 
-ERR = ["N/A", 99.0, 99.0, 99.0, 99.0] #initializes an array to hold error values for individual columns and the date error values that can't be automated #!Structure?
+#!ERR = ["N/A", 99.0, 99.0, 99.0, 99.0] #initializes an array to hold error values for individual columns and the date error values that can't be automated #!Structure?
 
 startYear = 1982
 endYear = 2021
 
-function NOAAcsvStitching(bouyID, startYear, endYear, writeOut = false)
-    
-    ERR = ["N/A", 99.0, 99.0, 99.0, 99.0] #initializes an array to hold error values for individual columns and the date error values that can't be automated #!Structure?
+neededColumns = pop![DefaultColumns]
 
-    str_BouyID = string(bouyID)
-    str_startYear = string(startYear)
+function NOAAcsvStitching(buoyID, startYear, endYear, writeOut = false)
+    
+    ERR = [] #initializes an array to hold error values for individual columns
+
+    str_buoyID = string(buoyID)
+    #!str_startYear = string(startYear)
 
     h = "h" #This is an h, its just a part of the names from the NDBC maybe it means hourly?
     undscore ="_" #For new naming convention
+    txt = ".txt" #matches the file type of the text files from NDBC
 
-    fullDF = CSV.read(str_BouyID*h*str_startYear.txt, DataFrame, header=1, delim=" ", ignorerepeated=true) #Initialize dataframe
-    
-    #insertcols!(fullDF, 5, :mm_mn => 99.0) #adds an error filled minute column to match structure of later years #!Structure?
-    #insertcols!(fullDF, 18, :TIDE_ft => 99.00) #adds an error filled tide column to match structure of later years #!Structure?
-    
-    #if startYear < 1999
-    #    fullDF[!,1] .+= 1900
-    #end
+    fullDF = data.frame(matrix(ncol = 18, nrow = 0))
+    colnames(fullDF) = 
 
-    #allowmissing!(fullDF)
-
-    #CSV.write("44007_1982.csv", fullDF)
-
-    #for col in 6:ncol(fullDF) #runs through columns of dataframe excluding the date
-    #    append!(ERR, maximum(fullDF[!,col])) #adds the max value of the column to the ERR array presumably the error value
-    #end
+    #! fullDF = CSV.read(str_BouyID*h*str_startYear.txt, DataFrame, header=1, delim=" ", ignorerepeated=true) #Initialize dataframe
 
     println(ERR) #prints error to make sure nothing untoward is included
 
-    for col in 2:ncol(fullDF)#runs through the columns of the dataframe excuding the year 
-        replace!(fullDF[!,col], ERR[col] => missing) #replaces the error value for each row with missing
+    for i in 2:length(defaultColumns)
+       append!(ERR, defaultColumns[i].errorValue)
     end
 
-    for i in length(defaultColumns)
-    
-    end
-
-    for runningYear in startYear+1:endYear  #Loop through number of years excluding the first as that is the year used to intialize the array
+    for runningYear in startYear:endYear  #Loop through number of years excluding the first as that is the year used to intialize the array
 
         yearName = string(runningYear) #turns year into a string
-        pullName = buoyID*h*yearName*txt  #Assemble file name for the year in acordance with NDBC
+        pullName = str_buoyID*h*yearName*txt  #Assemble file name for the year in acordance with NDBC
 
         if runningYear > 2006  #account for the move from one row of headers to two during the year 2006
             workingDF = CSV.read(pullName, DataFrame, header=1:2, delim=" ", ignorerepeated=true)
-        else
+        elseif runningYear > startYear
             workingDF = CSV.read(pullName, DataFrame, header=1, delim=" ", ignorerepeated=true)
         end
 
