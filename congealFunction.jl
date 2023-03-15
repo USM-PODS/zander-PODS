f425d1b1412bf010083b79825bf810aef46fe5cf
Zander Solak <zander.solak@gmail.com>
2023-03-15 13:05:31 -0400

    datamodule year error is now vector{any}

--- congealFunction.jl
+++ congealFunction.jl
@@ -3,36 +3,37 @@ using DataFrames
 
 include("DataModulePODS.jl")
 
-#!buoyID = "44007" #the ID number for the buoy
-txt = ".txt" #matches the file type of the text files from NDBC
-h = "h" #This is an h, its just a part of the names from the NDBC maybe it means hourly?
-undscore ="_" #For new naming convention
-commasep = ".csv" #used to write out files as comma seperated
+#! buoyID = "44007" #the ID number for the buoy
+#! txt = ".txt" #matches the file type of the text files from NDBC
+#! h = "h" #This is an h, its just a part of the names from the NDBC maybe it means hourly?
+#! undscore ="_" #For new naming convention
+#! commasep = ".csv" #used to write out files as comma seperated
 
-#!ERR = ["N/A", 99.0, 99.0, 99.0, 99.0] #initializes an array to hold error values for individual columns and the date error values that can't be automated #!Structure?
+#! ERR = ["N/A", 99.0, 99.0, 99.0, 99.0] #initializes an array to hold error values for individual columns and the date error values that can't be automated #!Structure?
 
 startYear = 1982
 endYear = 2021
-
-neededColumns = pop![DefaultColumns]
 
 function NOAAcsvStitching(buoyID, startYear, endYear, writeOut = false)
     
     ERR = [] #initializes an array to hold error values for individual columns
 
     str_buoyID = string(buoyID)
-    #!str_startYear = string(startYear)
+    
+    #! str_startYear = string(startYear)
 
     h = "h" #This is an h, its just a part of the names from the NDBC maybe it means hourly?
     undscore ="_" #For new naming convention
     txt = ".txt" #matches the file type of the text files from NDBC
+    commasep = ".csv" #used to write out files as comma seperated
 
     fullDF = data.frame(matrix(ncol = 18, nrow = 0))
-    colnames(fullDF) = 
+    
+    colnames(fullDF) = DefaultColumns
 
     #! fullDF = CSV.read(str_BouyID*h*str_startYear.txt, DataFrame, header=1, delim=" ", ignorerepeated=true) #Initialize dataframe
 
-    println(ERR) #prints error to make sure nothing untoward is included
+    #! println(ERR) #prints error to make sure nothing untoward is included
 
     for i in 2:length(defaultColumns)
        append!(ERR, defaultColumns[i].errorValue)
@@ -49,15 +50,15 @@ function NOAAcsvStitching(buoyID, startYear, endYear, writeOut = false)
             workingDF = CSV.read(pullName, DataFrame, header=1, delim=" ", ignorerepeated=true)
         end
 
-        if runningYear <2005  #add a column full of error values to years before 2005 to account for lack of minutes
+        if runningYear < 2005  #add a column full of error values to years before 2005 to account for lack of minutes
             insertcols!(workingDF, 5, :mm_mn => 99.0)
         end
 
-        if runningYear <2000 #add a column full of error values to years before 2000 to account for lack of TIDE
+        if runningYear < 2000 #add a column full of error values to years before 2000 to account for lack of TIDE
             insertcols!(workingDF, 18, :TIDE_ft => 99.00)
         end
 
-        if runningYear <1999
+        if runningYear < 1999
             workingDF[!,1] .+= 1900
         end
 
@@ -71,15 +72,17 @@ function NOAAcsvStitching(buoyID, startYear, endYear, writeOut = false)
             replace!(workingDF[!,col], ERR[col] => missing) #replaces each column's error values with missing
         end
 
-        pushName = buoyID*undscore*yearName*commasep #sets a new name for the new comma separated files
+        if writeOut == true
+            pushName = str_buoyID*undscore*yearName*commasep #sets a new name for the new comma separated files
 
-        #CSV.write(pushName, workingDF)
+            CSV.write(pushName, workingDF)
+        end
 
         append!(fullDF, workingDF) #attaches the corrected year to the running dataframe including all years
     end
-
+    
+        if writeOut == true
+            CSV.write("44007_1982-2021.csv", fullDF)
+        end
     return fullDF
 end
-
-
-CSV.write("44007_1982-2021.csv", fullDF)
